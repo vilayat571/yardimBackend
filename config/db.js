05c110f309d0530ac55244ee505f02786d445510
vilayat571 <vilayat571@gmail.com>
2026-03-14 16:22:22 +0800

    feat: update text

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
   } catch (error) {
-    console.log("❌ MongoDB connection failed:", error.message);
+    console.log("❌ MongoDB connection fail:", error.message);
     process.exit(1);
   }
 };
